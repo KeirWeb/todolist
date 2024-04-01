bd8eb7770b646cb08db85739bcd3c8770ada3487
keirWeb <zavalyuk00@list.ru>
2024-04-01 14:19:19 +0200

    fix API-KEY

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from "axios"
 
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
   headers: {
-    "API-KEY": "1cdd9f77-c60e-4af5-b194-659e4ebd5d41",
+    "API-KEY": "f09daa5b-2834-4b3b-81d6-287ed58b49b1",
   },
-});
+})
